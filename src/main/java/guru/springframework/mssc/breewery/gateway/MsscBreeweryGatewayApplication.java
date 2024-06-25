622b16d4a04708c370dfe562d4cc1f3042e08ec7
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-06-25 14:56:46 +0200

    first commit

---
+++ src/main/java/guru/springframework/mssc/breewery/gateway/MsscBreeweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.breewery.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreeweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreeweryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/mssc/breewery/gateway/MsscBreeweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.breewery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreeweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
