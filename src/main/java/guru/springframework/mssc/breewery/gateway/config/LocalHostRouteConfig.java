c79bb225aa53a6f3ff2ebb04af057dde0e56b6c6
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-06-25 15:08:43 +0200

    Add localHostRoutes

---
+++ src/main/java/guru/springframework/mssc/breewery/gateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.mssc.breewery.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(route -> route.path("/api/v1/beers*", "/api/v1/beers/*")
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .build();
+    }
+
+}
