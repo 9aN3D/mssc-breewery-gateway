28f075fcfd9de124a747a6a6acc58a31abe5e0be
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-06-29 18:35:39 +0200

    Add route for beer-order-service

--- src/main/java/guru/springframework/mssc/breewery/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/springframework/mssc/breewery/gateway/config/LocalHostRouteConfig.java
@@ -14,6 +14,9 @@ public class LocalHostRouteConfig {
                 .route(route -> route.path("/api/v1/beers*", "/api/v1/beers/*")
                         .uri("http://localhost:8080")
                         .id("beer-service"))
+                .route(route -> route.path("/api/v1/customers*")
+                        .uri("http://localhost:8081")
+                        .id("beer-order-service"))
                 .build();
     }
 
